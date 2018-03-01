512b424b743d49d0aa415b644353e539e90014ac
Pascool <pthomas.prophessi@gmail.com>
2018-03-01 14:18:51 +0100

    [COSMETIQUE] - Ajout footer LR (à fignoler au besoin)

--- src/main/webapp/app/custom/saisie-temps/saisie-temps.component.ts
+++ src/main/webapp/app/custom/saisie-temps/saisie-temps.component.ts
@@ -13,33 +13,17 @@ export class SaisieTempsComponent implements OnInit {
     rangeDates: Date[];
     minDate: Date;
     maxDate: Date;
-    es: any;
-    invalidDates: Array<Date>
-
+    invalidDates: Array<Date>;
 
     ngOnInit() {
-        this.es = {
-            firstDayOfWeek: 1,
-            dayNames: [ 'domingo','lunes','martes','miércoles','jueves','viernes','sábado' ],
-            dayNamesShort: [ 'dom','lun','mar','mié','jue','vie','sáb' ],
-            dayNamesMin: [ 'D','L','M','X','J','V','S' ],
-            monthNames: [ 'enero','febrero','marzo','abril','mayo','junio','julio','agosto','septiembre','octubre','noviembre','diciembre' ],
-            monthNamesShort: [ 'ene','feb','mar','abr','may','jun','jul','ago','sep','oct','nov','dic' ],
-            today: 'Hoy',
-            clear: 'Borrar'
-        }
 
-      // this.tr = {
-      //     firstDayOfWeek : 1
-      // }
-
-        let today = new Date();
-        let month = today.getMonth();
-        let year = today.getFullYear();
-        let prevMonth = (month === 0) ? 11 : month -1;
-        let prevYear = (prevMonth === 11) ? year - 1 : year;
-        let nextMonth = (month === 11) ? 0 : month + 1;
-        let nextYear = (nextMonth === 0) ? year + 1 : year;
+        const today = new Date();
+        const month = today.getMonth();
+        const year = today.getFullYear();
+        const prevMonth = (month === 0) ? 11 : month - 1;
+        const prevYear = (prevMonth === 11) ? year - 1 : year;
+        const nextMonth = (month === 11) ? 0 : month + 1;
+        const nextYear = (nextMonth === 0) ? year + 1 : year;
         this.minDate = new Date();
         this.minDate.setMonth(prevMonth);
         this.minDate.setFullYear(prevYear);
@@ -47,9 +31,9 @@ export class SaisieTempsComponent implements OnInit {
         this.maxDate.setMonth(nextMonth);
         this.maxDate.setFullYear(nextYear);
 
-        let invalidDate = new Date();
+        const invalidDate = new Date();
         invalidDate.setDate(today.getDate() - 1);
-        this.invalidDates = [today,invalidDate];
+        this.invalidDates = [today, invalidDate];
     }
 
 }
